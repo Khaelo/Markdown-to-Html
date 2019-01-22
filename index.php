62aa39725a22c24aefbd3a93957b82f57fb669da
Luca Bruno <luca.bruno@ynov.com>
2019-01-22 20:44:43 +0100

    Update

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
 		$file_tmp_name = $_FILES['fichier']['tmp_name'];
 
 		$extensions_autorisees = array('.md', '.MD');
-		$file_dest = '../md/'.$file_name;
+		$file_dest = tempnam('md/', "md_");
 
 		if(in_array($file_extension, $extensions_autorisees)){
 			if(move_uploaded_file($file_tmp_name, $file_dest)){
-				echo 'Files upload success!';
+				exec('py/convert.py');
 			} else {
 				echo 'Error';
 			}
@@ -23,19 +23,20 @@
 <head>
 	<meta charset="utf-8" />
     <meta lang="fr" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 	<title>Markdown to HTML</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<link rel="stylesheet" href="include/styles.css">
+	<link rel="stylesheet" href="includes/css/styles.css">
 </head>
 <body>
 	<h1 class="text-center align-middle">Markdown to HTML</h1>
 	<form method="post" action="" enctype="multipart/form-data">
-		<div>
-			<label for="file"><input type="file" name="fichier" id="md" value="Select a File" accept=".md"></label>
+		<div class="upload-btn-wrapper">
+			<button class="btn">Upload a file</button>
+			<input type="file" name="fichier" id="md" accept=".md">
 		</div>
 		<div>
-			<button>Submit</button>	
+			<button class="btn-submit btn-success">Submit</button>
 		</div>
 	</form>
 </body>
---
+++ py/convert.py
@@ -0,0 +1,5 @@
+import os, sys
+
+file = 
+
+os.remove(file)
