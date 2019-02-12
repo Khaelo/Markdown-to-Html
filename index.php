9a8fab41ab8527d643485de2066c5ec4d62ed613
Luca Bruno <luca.bruno@ynov.com>
2019-02-12 08:31:15 +0100

    Done !

--- index.php
+++ index.php
@@ -1,22 +1,27 @@
 <?php
+	$files_error = "";
+	$file_dest_html = "html/index.html";
 	if(!empty($_FILES)){
 		$file_name = $_FILES['fichier']['name'];
 		$file_extension = strrchr($file_name, ".");
 		$file_tmp_name = $_FILES['fichier']['tmp_name'];
 
 		$extensions_autorisees = array('.md', '.MD');
-		$file_dest = tempnam('md/', "md_");
+		$file_dest = "md/$file_name";
+
 
 		if(in_array($file_extension, $extensions_autorisees)){
 			if(move_uploaded_file($file_tmp_name, $file_dest)){
-				exec('py/convert.py');
+				shell_exec("python py/convert.py $file_name -o $file_dest_html");
+				header('Refresh: 2;URL=..');
 			} else {
-				echo 'Error';
+				$files_error = "Error";
 			}
 		} else {
-			echo 'Seuls les fichiers MD sont autorisés !';
+			$files_error = "Seulement les fichiers MD sont acceptés";
 		}
-	}
+	}		
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -27,6 +32,7 @@
 	<title>Markdown to HTML</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" href="includes/css/styles.css">
+	<link rel="stylesheet" href="includes/css/prism.css">
 </head>
 <body>
 	<h1 class="text-center align-middle">Markdown to HTML</h1>
@@ -38,6 +44,25 @@
 		<div>
 			<button class="btn-submit btn-success">Submit</button>
 		</div>
-	</form>
+	</form><br/>
+		<?php if($files_error && $files_error){
+			echo '<div class="alert alert-warning text-center" role="alert">'; 
+			echo $files_error.'</div>';
+		}
+		?>
+	<div class="language-html">
+	<?php 
+			$fichierR =  fopen("$file_dest_html","r");
+			echo '<pre class="language-html code" ><code>';
+			while(!feof($fichierR)){
+				$convert = fgets($fichierR);
+				$text = htmlentities($convert);
+				echo $text;
+			}
+			echo '</pre></code>';
+			fclose($fichierR)
+		?>
+	</div>
+	<script src="includes/js/prism.js"></script>
 </body>
 </html>
